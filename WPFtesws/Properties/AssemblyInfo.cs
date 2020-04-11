c4b67bb056774fefb46b0a8d5801eb64446eb732
gxh <46601807+littlegao233@users.noreply.github.com>
2020-04-11 21:03:11 +0800

    complete

--- WPFtesws/Properties/AssemblyInfo.cs
+++ WPFtesws/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("WPFtesws")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WPFWSC")]
+[assembly: AssemblyDescription("For BDX-WS have a better test environment")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WPFtesws")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("WPFWSC")]
+[assembly: AssemblyCopyright("Copyright © gxh 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.*")]
--- WPFtesws/tesws.xaml.cs
+++ WPFtesws/tesws.xaml.cs
@@ -1,10 +1,12 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using MaterialDesignThemes.Wpf;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,6 +30,14 @@ namespace MinecraftToolKit.Pages
         }
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => Binding.DoNothing;
     }
+    public class Up0ToBool : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int)value > 0;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => Binding.DoNothing;
+    }
     //public class SelectedClientToBool : IValueConverter
     //{
     //    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -44,6 +54,10 @@ namespace MinecraftToolKit.Pages
         public Tesws()
         {
             InitializeComponent();
+            //if (true)
+            //{
+
+            //}
             if (File.Exists(Environment.CurrentDirectory + "\\tesws.json"))
             {
                 OutPut($"检测到位于{Environment.CurrentDirectory}\\tesws.json的配置文件");
@@ -54,39 +68,103 @@ namespace MinecraftToolKit.Pages
                         WebSocket webSocket = new WebSocket(item["Address"].ToString());
                         webSocket.OnOpen += (senderClient, ClientE) =>
                         {
-                            OutPut($"{(senderClient as WebSocket).Url}已连接");
+                            OutPut((senderClient as WebSocket).Url, $"已连接");
                         };
                         webSocket.OnError += (senderClient, ClientE) =>
                         {
-                            OutPut($"{(senderClient as WebSocket).Url}出错啦{ClientE.Message}");
+                            OutPutErr((senderClient as WebSocket).Url, $"出错啦{ClientE.Message}");
                         };
                         webSocket.OnMessage += WebSocket_OnMessage;
                         webSocket.OnClose += (senderClient, ClientE) =>
                         {
-                            OutPut($"{(senderClient as WebSocket).Url}连接已关闭\t{ClientE.Code}=>{ClientE.Reason}");
+                            OutPutErr((senderClient as WebSocket).Url, $"连接已关闭\t{ClientE.Code}=>{ClientE.Reason}");
                         };
                         SelectServer.Items.Add(new ComboBoxItem() { Content = item["Address"].ToString(), Tag = new WS() { client = webSocket, info = item } });
                     }
                     OutPut("配置文件读取成功");
                 }
                 catch (Exception err)
-                { OutPut("配置文件读取失败" + err.Message); }
+                { OutPutErr("配置文件读取失败" + err.Message); }
             }
         }
-        #region 艹
+        #region 添加/编辑
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                JObject jObject = new JObject() { new JProperty("Servers", new JArray()) };
+                foreach (var item in SelectServer.Items)
+                {
+                    ((JArray)jObject["Servers"]).Add(((WS)((ComboBoxItem)item).Tag).info);
+                }
+                File.WriteAllText(Environment.CurrentDirectory + "\\tesws.json", jObject.ToString());
+                OutPut("配置文件已保存到" + Environment.CurrentDirectory + "\\tesws.json");
+            }
+            catch (Exception) { }
+        }
+        private bool AddMode = true;
         private void AddServerButton_Click(object sender, RoutedEventArgs e)
         {
-            //DialogContent.Children.Clear();
-            //string get = null;
             AddAddress.Clear();
             AddPWD.Clear();
             AddTip.Visibility = Visibility.Collapsed;
             Dialog.IsOpen = true;
-
-            //DialogContent.Children.Add(new Button() {});
-
+            AddMode = true;
         }
-        #endregion
+        private void EditServerButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddAddress.Text = ((WS)((ComboBoxItem)SelectServer.SelectedItem).Tag).info["Address"].ToString();
+            AddPWD.Text = ((WS)((ComboBoxItem)SelectServer.SelectedItem).Tag).info["Password"].ToString();
+            AddTip.Visibility = Visibility.Collapsed;
+            Dialog.IsOpen = true;
+            AddMode = false;
+        }
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                WebSocket webSocket = new WebSocket(AddAddress.Text);
+                webSocket.OnOpen += (senderClient, ClientE) =>
+                {
+                    OutPut((senderClient as WebSocket).Url, $"已连接");
+                };
+                webSocket.OnError += (senderClient, ClientE) =>
+               {
+                   OutPutErr((senderClient as WebSocket).Url, $"出错啦{ClientE.Message}");
+               };
+                webSocket.OnMessage += WebSocket_OnMessage;
+                webSocket.OnClose += (senderClient, ClientE) =>
+                {
+                    OutPut((senderClient as WebSocket).Url, $"连接已关闭\t{ClientE.Code}=>{ClientE.Reason}");
+                };
+                if (AddMode)
+                {
+                    SelectServer.Items.Add(new ComboBoxItem() { Content = AddAddress.Text, Tag = new WS() { client = webSocket, info = new JObject() { new JProperty("Address", AddAddress.Text), new JProperty("Password", AddPWD.Text) } } });
+                    if (SelectServer.SelectedIndex == -1) { SelectServer.SelectedIndex = 0; }
+                    OutPut($"添加成功=>{AddAddress.Text}");
+                }
+                else
+                {
+                    if (((WS)((ComboBoxItem)SelectServer.SelectedItem).Tag).client.IsAlive)
+                    { ((WS)((ComboBoxItem)SelectServer.SelectedItem).Tag).client.CloseAsync(); }
+                    SelectServer.Items.Remove(SelectServer.SelectedItem);
+                    SelectServer.Items.Add(new ComboBoxItem() { Content = AddAddress.Text, Tag = new WS() { client = webSocket, info = new JObject() { new JProperty("Address", AddAddress.Text), new JProperty("Password", AddPWD.Text) } } });
+                    SelectServer.SelectedIndex = SelectServer.Items.Count - 1;
+                    OutPut($"修改成功=>{AddAddress.Text}");
+                }
+                Dialog.IsOpen = false;
+            }
+            catch (Exception err)
+            {
+                AddTip.Visibility = Visibility.Visible;
+                AddTip.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                AddTip.Text = "填写内容不合规\n" + err.Message;
+            }
+        }
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Dialog.IsOpen = false;
+        }
         private void RemoveServerButton_Click(object sender, RoutedEventArgs e)
         {
             if (((WS)((ComboBoxItem)SelectServer.SelectedItem).Tag).client.IsAlive)
@@ -95,7 +173,20 @@ namespace MinecraftToolKit.Pages
             };
             SelectServer.Items.Remove(SelectServer.SelectedItem);
         }
-        public static JObject GetCmdReq(string token, string cmd)
+        private void CleanButton_Click(object sender, RoutedEventArgs e)
+        {
+            OutPutText.Document.Blocks.Clear();
+        }
+        #endregion
+        public void UnInstall()
+        {
+            foreach (var item in SelectServer.Items)
+            {
+                ((WS)((ComboBoxItem)item).Tag).client.Close();
+                this.Content = null;
+            }
+        }
+        public JObject GetCmdReq(string token, string cmd)
         {
             string GetMD5(string sDataIn)
             {
@@ -111,7 +202,7 @@ namespace MinecraftToolKit.Pages
                 }
                 return sTemp.ToUpper();
             }
-            JObject raw = JObject.Parse("{\"op\":\"runcmd\",\"passwd\":\"token\",\"cmd\":\"say null\"}");
+            JObject raw = JObject.Parse("{\"operate\":\"runcmd\",\"passwd\":\"token\",\"cmd\":\"say null\"}");
             raw["cmd"] = cmd;
             raw["passwd"] = GetMD5(token + DateTime.Now.ToString("yyyyMMddHHmm"));
             return raw;
@@ -126,7 +217,7 @@ namespace MinecraftToolKit.Pages
                     {
                         case 0:
                             string getStr = SendText.Text.Replace("%pwd%", GetCmdReq(((WS)((ComboBoxItem)SelectServer.SelectedItem).Tag).info["Password"].ToString(), null)["passwd"].ToString());
-                            ((WS)((ComboBoxItem)SelectServer.SelectedItem).Tag).client.SendAsync(getStr, (state) => OutPut(state ? "发送成功=>" + SendText.Text : "发送失败!?"));
+                            ((WS)((ComboBoxItem)SelectServer.SelectedItem).Tag).client.SendAsync(getStr, (state) => OutPut(state ? "发送成功=>" + getStr : "发送失败!?"));
                             OutPut(getStr);
                             break;
                         case 1:
@@ -135,14 +226,18 @@ namespace MinecraftToolKit.Pages
                             //SendText.Text 
                             break;
                         default:
-                            OutPut("还没选择消息内容类别啊啊啊啊啊啊！！！");
+                            OutPutErr("还没选择消息内容类别啊啊啊啊啊啊！！！");
                             break;
                     }
                 }
                 else
                 {
-                    OutPut("还没连接啊啊啊啊啊啊！！！");
+                    OutPutErr("还没连接啊啊啊啊啊啊！！！");
                 }
+            }
+            else
+            {
+                OutPutErr("把你想要发送的Server选上再点啊啊啊啊啊啊！！！");
             }
         }
         public struct WS
@@ -150,49 +245,98 @@ namespace MinecraftToolKit.Pages
             public WebSocket client;
             public JObject info;
         }
-        private void OutPut(object input) => Dispatcher.Invoke(() => { OutPutText.AppendText(DateTime.Now.ToString() + ">" + input + Environment.NewLine); OutPutText.ScrollToEnd(); });
-        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        #region OUTPUT
+        private void OutPut(object input) => Dispatcher.Invoke(() =>
+        {
+            OutPutText.Document.Blocks.Add(new Paragraph(new Run(DateTime.Now.ToString()) { Foreground = Brushes.Blue }));
+            Paragraph paragraph = new Paragraph();
+            paragraph.Inlines.Add(new PackIcon() { Kind = PackIconKind.ConsoleLine, Margin = new Thickness(0, -3, 0, -3), Foreground = Brushes.Purple });
+            paragraph.Inlines.Add(new Run(input.ToString()) { Foreground = Brushes.Black });
+            OutPutText.Document.Blocks.Add(paragraph);
+            OutPutText.ScrollToEnd();
+        });
+        private void OutPutErr(object input) => Dispatcher.Invoke(() =>
+        {
+            OutPutText.Document.Blocks.Add(new Paragraph(new Run(DateTime.Now.ToString()) { Foreground = Brushes.Blue }));
+            Paragraph paragraph = new Paragraph();
+            paragraph.Inlines.Add(new PackIcon() { Kind = PackIconKind.ErrorOutline, Margin = new Thickness(0, -3, 0, -3), Foreground = Brushes.Red });
+            paragraph.Inlines.Add(new Run(input.ToString()) { Foreground = Brushes.Red });
+            OutPutText.Document.Blocks.Add(paragraph);
+            OutPutText.ScrollToEnd();
+        });
+        private void OutPutErr(Uri from, object input) => Dispatcher.Invoke(() =>
+        {
+            Paragraph paragraphFrom = new Paragraph();
+            paragraphFrom.Inlines.Add(new Run(DateTime.Now.ToString()) { Foreground = Brushes.Blue });
+            paragraphFrom.Inlines.Add(new Run("=>") { Foreground = Brushes.Yellow });
+            paragraphFrom.Inlines.Add(new Run($"From {from.ToString()}") { Foreground = Brushes.Green });
+            OutPutText.Document.Blocks.Add(paragraphFrom);
+            Paragraph paragraph = new Paragraph();
+            paragraph.Inlines.Add(new PackIcon() { Kind = PackIconKind.ErrorOutline, Margin = new Thickness(0, -3, 0, -3), Foreground = Brushes.Red });
+            paragraph.Inlines.Add(new Run(input.ToString()) { Foreground = Brushes.Red });
+            OutPutText.Document.Blocks.Add(paragraph);
+            OutPutText.ScrollToEnd();
+        });
+        private void OutPut(Uri from, string type, object input) => Dispatcher.Invoke(() =>
+        {
+            Paragraph paragraphFrom = new Paragraph();
+            paragraphFrom.Inlines.Add(new Run(DateTime.Now.ToString()) { Foreground = Brushes.Blue });
+            paragraphFrom.Inlines.Add(new Run("=>") { Foreground = Brushes.Yellow });
+            paragraphFrom.Inlines.Add(new Run($"From {from.ToString()}") { Foreground = Brushes.Green });
+            OutPutText.Document.Blocks.Add(paragraphFrom);
+            Paragraph paragraph = new Paragraph();
+            paragraph.Inlines.Add(new PackIcon() { Kind = PackIconKind.ConsoleLine, Margin = new Thickness(0, -3, 0, -3), Foreground = Brushes.Purple });
+            paragraph.Inlines.Add(new Run(type.ToString()) { Foreground = Brushes.DarkMagenta });
+            OutPutText.Document.Blocks.Add(paragraph);
+            Paragraph paragraph1 = new Paragraph();
+            paragraph1.Inlines.Add(new Run(Regex.Replace(input.ToString(), "\n+$", "")) { Foreground = Brushes.Gray });
+            OutPutText.Document.Blocks.Add(paragraph1);
+            OutPutText.ScrollToEnd();
+        });
+        private void OutPut(Uri from, object input) => Dispatcher.Invoke(() =>
+        {
+            Paragraph paragraphFrom = new Paragraph();
+            paragraphFrom.Inlines.Add(new Run(DateTime.Now.ToString()) { Foreground = Brushes.Blue });
+            paragraphFrom.Inlines.Add(new Run("=>") { Foreground = Brushes.Yellow });
+            paragraphFrom.Inlines.Add(new Run($"From {from.ToString()}") { Foreground = Brushes.Green });
+            OutPutText.Document.Blocks.Add(paragraphFrom);
+            Paragraph paragraph = new Paragraph();
+            paragraph.Inlines.Add(new PackIcon() { Kind = PackIconKind.ConsoleLine, Margin = new Thickness(0, -3, 0, -3), Foreground = Brushes.Purple });
+            paragraph.Inlines.Add(new Run(Regex.Replace(input.ToString(), "\n+$", "")) { Foreground = Brushes.Gray });
+            OutPutText.Document.Blocks.Add(paragraph);
+            OutPutText.ScrollToEnd();
+        });
+        #endregion
+        private void WebSocket_OnMessage(object sender, MessageEventArgs e)
         {
             try
             {
-                WebSocket webSocket = new WebSocket(AddAddress.Text);
-                webSocket.OnOpen += (senderClient, ClientE) =>
+                JObject receive = JObject.Parse(e.Data);
+                switch (receive["operate"].ToString())
                 {
-                    OutPut($"{(senderClient as WebSocket).Url}已连接");
-                };
-                webSocket.OnError += (senderClient, ClientE) =>
-               {
-                   OutPut($"{(senderClient as WebSocket).Url}出错啦{ClientE.Message}");
-               };
-                webSocket.OnMessage += WebSocket_OnMessage;
-                webSocket.OnClose += (senderClient, ClientE) =>
-                {
-                    OutPut($"{(senderClient as WebSocket).Url}连接已关闭\t{ClientE.Code}=>{ClientE.Reason}");
-                };
-                SelectServer.Items.Add(new ComboBoxItem() { Content = AddAddress.Text, Tag = new WS() { client = webSocket, info = new JObject() { new JProperty("Address", AddAddress.Text), new JProperty("Password", AddPWD.Text) } } });
-                if (SelectServer.SelectedIndex == -1)
-                {
-                    SelectServer.SelectedIndex = 0;
+                    case "runcmd":
+                        if (receive["Auth"].ToString() == "Failed")
+                        {
+                            OutPut(((WebSocket)sender).Url, "命令执行反馈", "密码不匹配！！！");
+                        }
+                        else
+                        {
+                            OutPut(((WebSocket)sender).Url, "命令执行反馈", receive["text"]);
+                        }
+                        return;
+                    default:
+                        break;
                 }
-                Dialog.IsOpen = false;
             }
-            catch (Exception err)
+            catch (Exception) { }
+            try
             {
-                AddTip.Visibility = Visibility.Visible;
-                AddTip.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
-                AddTip.Text = "填写内容不合规\n" +
-                 err.Message;
+                OutPut((sender as WebSocket).Url, $"收信:\n{JObject.Parse(e.Data)}");
             }
-        }
-
-        private void WebSocket_OnMessage(object sender, MessageEventArgs e)
-        {
-            OutPut($"{(sender as WebSocket).Url}收信:{e.Data}");
-        }
-
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
-        {
-            Dialog.IsOpen = false;
+            catch (Exception)
+            {
+                OutPutErr((sender as WebSocket).Url, $"解析失败:{e.Data}");
+            }
         }
         private void SelectServer_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -204,32 +348,18 @@ namespace MinecraftToolKit.Pages
             {
                 if (((WS)((ComboBoxItem)SelectServer.SelectedItem).Tag).client.IsAlive)
                 {
-                    OutPut("已经连上了啊啊啊啊啊啊！！！");
+                    OutPutErr("已经连上了啊啊啊啊啊啊！！！");
                 }
                 else
                 {
+                    OutPut("正在尝试连接至" + ((WS)((ComboBoxItem)SelectServer.SelectedItem).Tag).client.Url);
                     ((WS)((ComboBoxItem)SelectServer.SelectedItem).Tag).client.ConnectAsync();
                 }
             }
             else
             {
-                OutPut("把你想要发送的Server选上再点啊啊啊啊啊啊！！！");
+                OutPutErr("把你想要发送的Server选上再点啊啊啊啊啊啊！！！");
             }
-        }
-
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                JObject jObject = new JObject() { new JProperty("Servers", new JArray()) };
-                foreach (var item in SelectServer.Items)
-                {
-                    ((JArray)jObject["Servers"]).Add(((WS)((ComboBoxItem)item).Tag).info);
-                }
-                File.WriteAllText(Environment.CurrentDirectory + "\\tesws.json", jObject.ToString());
-                OutPut("配置文件已保存到" + Environment.CurrentDirectory + "\\tesws.json");
-            }
-            catch (Exception) { }
         }
     }
 }
