7d053413946afe8b982fb1498606a4018017015c
gxh <46601807+littlegao233@users.noreply.github.com>
2020-05-03 04:04:48 +0800

    Final

--- WPFtesws/MainWindow.xaml.cs
+++ WPFtesws/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignExtensions.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace WPFtesws
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MaterialWindow
     {
         public MainWindow()
         {
